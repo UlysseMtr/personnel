8e376691c91e4b96fe2eefea1dc0b81c07477297
Abdullah-Asci <abdullah.asci1702@gmail.com>
2024-10-07 14:51:46 +0200

    ajout test pour employe

---
+++ Personnel/src/testsUnitaires/TestEmploye.java
@@ -0,0 +1,21 @@
+package testsUnitaires;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import personnel.*;
+
+public class TestEmploye {
+	
+	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+	
+	@Test
+	void createEmploye() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
+		assertEquals("Bouchard", employe.getNom());
+		assertEquals("Gérard", employe.getPrenom());
+		assertEquals("g.bouchard@gmail.com", employe.getMail());
+		assertEquals(1, employe.checkPassword("azerty"));
+	}
