7252849f804ba97c63fcab8a121b79d8c15d0aa0
Abdullah-Asci <144663894+Abdullah-Asci@users.noreply.github.com>
2024-11-18 17:18:43 +0100

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Ligue(
-   id_ligue VARCHAR(50),
+   id_ligue INT,
    PRIMARY KEY(id_ligue)
 );
 
 CREATE TABLE Employe(
-   id_employe VARCHAR(50),
-   nom VARCHAR(50),
-   prenom VARCHAR(50),
-   password VARCHAR(50),
+   id_employe INT,
+   nom VARCHAR(30),
+   prenom VARCHAR(30),
    mail VARCHAR(50),
-   status VARCHAR(50),
+   password VARCHAR(30),
+   status VARCHAR(20),
    dateArrivee DATE,
    dateDepart DATE,
-   id_ligue VARCHAR(50),
+   id_ligue INT,
    PRIMARY KEY(id_employe),
    FOREIGN KEY(id_ligue) REFERENCES Ligue(id_ligue)
 );
