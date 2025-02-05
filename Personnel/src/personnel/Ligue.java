2fb9cc93625863a26bd84792c490c55d4aa720d2
Ulysse <ulyssemetra@gmail.com>
2025-02-05 23:55:27 +0100

    Suppression de la double suppression des emplouÃyés

--- Personnel/src/personnel/Ligue.java
+++ Personnel/src/personnel/Ligue.java
@@ -2,7 +2,6 @@ package personnel;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -10,8 +9,8 @@ import java.util.TreeSet;
 /**
  * Représente une ligue. Chaque ligue est reliée à une liste
  * d'employés dont un administrateur. Comme il n'est pas possible
- * de créer un employé sans l'affecter à une ligue, le root est 
- * l'administrateur de la ligue jusqu'à ce qu'un administrateur 
+ * de créer un employé sans l'affecter à une ligue, le root est
+ * l'administrateur de la ligue jusqu'à ce qu'un administrateur
  * lui ait été affecté avec la fonction {@link #setAdministrateur}.
  */
 
@@ -23,16 +22,16 @@ public class Ligue implements Serializable, Comparable<Ligue>
 	private SortedSet<Employe> employes;
 	private Employe administrateur;
 	private GestionPersonnel gestionPersonnel;
-	
+
 	/**
 	 * Crée une ligue.
 	 * @param nom le nom de la ligue.
 	 */
-	
+
 	Ligue(GestionPersonnel gestionPersonnel, String nom) throws SauvegardeImpossible
 	{
 		this(gestionPersonnel, -1, nom);
-		this.id = gestionPersonnel.insert(this); 
+		this.id = gestionPersonnel.insert(this);
 	}
 
 	Ligue(GestionPersonnel gestionPersonnel, int id, String nom)
@@ -44,12 +43,12 @@ public class Ligue implements Serializable, Comparable<Ligue>
 		this.id = id;
 	}
 
-	
+
 	public int getIdLigue()
 	{
 		return id;
 	}
-	
+
 	/**
 	 * Retourne le nom de la ligue.
 	 * @return le nom de la ligue.
@@ -75,7 +74,7 @@ public class Ligue implements Serializable, Comparable<Ligue>
 	 * Retourne l'administrateur de la ligue.
 	 * @return l'administrateur de la ligue.
 	 */
-	
+
 	public Employe getAdministrateur()
 	{
 		return administrateur;
@@ -83,12 +82,12 @@ public class Ligue implements Serializable, Comparable<Ligue>
 
 	/**
 	 * Fait de administrateur l'administrateur de la ligue.
-	 * Lève DroitsInsuffisants si l'administrateur n'est pas 
-	 * un employé de la ligue ou le root. Révoque les droits de l'ancien 
+	 * Lève DroitsInsuffisants si l'administrateur n'est pas
+	 * un employé de la ligue ou le root. Révoque les droits de l'ancien
 	 * administrateur.
 	 * @param administrateur le nouvel administrateur de la ligue.
 	 */
-	
+
 	public void setAdministrateur(Employe administrateur) throws SauvegardeImpossible
 	{
 		Employe root = gestionPersonnel.getRoot();
@@ -102,33 +101,33 @@ public class Ligue implements Serializable, Comparable<Ligue>
 	 * Retourne les employés de la ligue.
 	 * @return les employés de la ligue dans l'ordre alphabétique.
 	 */
-	
+
 	public SortedSet<Employe> getEmployes()
 	{
 		return Collections.unmodifiableSortedSet(employes);
 	}
 
 	/**
-	 * Ajoute un employé dans la ligue. Cette méthode 
+	 * Ajoute un employé dans la ligue. Cette méthode
 	 * est le seul moyen de créer un employé.
 	 * @param nom le nom de l'employé.
 	 * @param prenom le prénom de l'employé.
 	 * @param mail l'adresse mail de l'employé.
 	 * @param password le password de l'employé.
-	 * @return l'employé créé. 
-	 * @throws SauvegardeImpossible 
-	 * @throws ExceptionDate 
+	 * @return l'employé créé.
+	 * @throws SauvegardeImpossible
+	 * @throws ExceptionDate
 	 */
-	
-	public Employe addEmploye(int id, String nom, String prenom, String mail, String password, 
-			LocalDate dateArrivee, LocalDate dateDepart, boolean admin) 
+
+	public Employe addEmploye(int id, String nom, String prenom, String mail, String password,
+			LocalDate dateArrivee, LocalDate dateDepart, boolean admin)
 			throws SauvegardeImpossible, ExceptionDate
 	{
 		Employe employe = new Employe(-1, gestionPersonnel, this, nom, prenom, mail, password, dateArrivee, dateDepart, admin);
 		employes.add(employe);
 		return employe;
 	}
-	
+
 	public Employe addEmploye(String nom, String prenom, String mail, String password, LocalDate dateArrivee, LocalDate dateDepart)
 			throws ExceptionDate
 			{
@@ -136,35 +135,31 @@ public class Ligue implements Serializable, Comparable<Ligue>
 				employes.add(employe);
 				return employe;
 			}
-	
-	
+
+
 	void remove(Employe employe)
 	{
 		employes.remove(employe);
 	}
-	
+
 	/**
 	 * Supprime la ligue, entraîne la suppression de tous les employés
 	 * de la ligue.
 	 */
-	
+
 	public void remove() throws SauvegardeImpossible
 	{
-		for (Employe employe : new ArrayList<>(employes))
-		{
-			employe.remove();
-		}
 		gestionPersonnel.delete(this);
 		gestionPersonnel.remove(this);
 	}
-	
+
 
 	@Override
 	public int compareTo(Ligue autre)
 	{
 		return getNom().compareTo(autre.getNom());
 	}
-	
+
 	@Override
 	public String toString()
 	{
